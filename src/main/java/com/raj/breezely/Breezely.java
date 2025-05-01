9af1f49c4974bd0a907ac59cc36730a86d108406
Rajendra Prasad Rout <rajendrarout8018@gmail.com>
2025-05-01 17:01:11 +0530

    Added env file and removed mongo uri from application.yml file

--- src/main/java/com/raj/breezely/Breezely.java
+++ src/main/java/com/raj/breezely/Breezely.java
@@ -1,13 +1,28 @@
 package com.raj.breezely;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class Breezely {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		System.setProperty("MONGO_URI", dotenv.get("MONGO_URI"));
+
 		SpringApplication.run(Breezely.class, args);
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager(MongoDatabaseFactory mongoDatabaseFactory) {
+		return new MongoTransactionManager(mongoDatabaseFactory);
 	}
 
 }
